643cc5d558e735df065686a69ab15ebe1ed211c6
Liam Bowie <liambowie16@gmail.com>
2022-12-21 13:28:38 +0000

    Passed hard coded searchResults from App down to TrackList component via SearchResults component and rendered them to the page

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,10 +3,20 @@ import { Track } from "../Track/Track";
 import './TrackList.css'
 
 export class TrackList extends React.Component { 
+
+    displayTracks() {
+        if(this.props.tracks !== undefined){
+            this.props.tracks.map(track => {
+                console.log(track.name);
+                return <li key={track.id}>{track.name}</li>
+            })
+        }
+    }
+
     render() { 
         return (
             <div className="TrackList">
-                {/* You will add a map method that renders a set of Track components */}
+                { this.props.tracks && this.props.tracks.map(track => <li key={track.id}>{track.name} - {track.artist} ({track.album})</li>) }
             </div>
         )
     }
