9e24d7250f02707726b4b9d0e1f7bcc774b8b5dd
Liam Bowie <liambowie16@gmail.com>
2022-12-21 11:32:40 +0000

    Created SearchBar Component with accompanying stylesheet

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 function App() {
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-
+        <SearchBar />
         <div className="App-playlist">
 
 
